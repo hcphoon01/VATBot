24aff499ca441499c7e146c72f4d3b0f7f5425db
Harry Cameron <hcphoon01@gmail.com>
2020-01-11 13:08:52 +0000

    disabled taf command to ammend

--- src/commands/Weather/TAF/dtaf.js
+++ src/commands/Weather/TAF/dtaf.js
@@ -1,5 +1,7 @@
 const { Command } = require("klasa");
+const Discord = require('discord.js');
 const request = require("request");
+const moment = require('moment');
 
 module.exports = class extends Command {
   constructor(...args) {
@@ -10,6 +12,7 @@ module.exports = class extends Command {
     super(...args, {
       name: "Decoded TAF",
       aliases: ["dtaf"],
+      enabled: false,
       cooldown: 2,
       description: "Get a decoded TAF for a given ICAO Code",
       usage: "<ICAO:icao>",
@@ -20,62 +23,72 @@ module.exports = class extends Command {
   async run(message, [airport]) {
     if (airport.length == 0) return message.reply("you must specify an ICAO code");
     request(
-      `https://api.checkwx.com/TAF/${airport}/decoded`,
+      `https://api.checkwx.com/taf/${airport}/decoded`,
       { headers: { "X-API-Key": process.env.WX_API }, json: true },
       (err, res, body) => {
         if (err) {
           console.log(err);
         }
         if (body.results !== 0) {
-          const embed = {
-            embed: {
-              title: `Decoded TAF for ${airport}`,
-              description: `RAW TAF: ` + "`" + body.data[0].raw_text + "`",
-              color: 4691726,
-              fields: [
-                {
-                  name: "Wind",
-                  value: body.data[0].wind
-                    ? `${body.data[0].wind.degrees} Degrees ${body.data[0].wind.speed_kts} Knots ${body.data[0].wind.speed_mph} miles per hour`
-                    : `Wind Calm`
-                },
-                {
-                  name: "Visibility",
-                  value: `${body.data[0].visibility.miles} miles ${body.data[0].visibility.meters} meters`,
-                  inline: true
-                },
-                {
-                  name: "Clouds",
-                  value: body.data[0].feet_agl
-                    ? `${body.data[0].ceiling.text} ${body.data[0].ceiling.feet_agl} feet ${body.data[0].ceiling.meters_agl} meters`
-                    : body.data[0].clouds[0]
-                    ? body.data[0].clouds[0].text
-                    : `Unknown Details`,
-                  inline: true
-                },
-                {
-                  name: "Temperature",
-                  value: `${body.data[0].temperature.celsius}°C ${body.data[0].temperature.fahrenheit}°F`,
-                  inline: true
-                },
-                {
-                  name: "Dewpoint",
-                  value: `${body.data[0].dewpoint.celsius}°C ${body.data[0].dewpoint.fahrenheit}°F`,
-                  inline: true
-                },
-                {
-                  name: "Barometer (QNH)",
-                  value: `${body.data[0].barometer.hpa} hPa ${body.data[0].barometer.hg} hg`,
-                  inline: true
-                },
-                {
-                  name: "Humidity",
-                  value: `${body.data[0].humidity.percent}%`,
-                  inline: true
-                }
-              ]
+          const embed = new Discord.MessageEmbed();
+          embed.setTitle(`Decoded TAF for ${airport}`)
+            .setDescription('RAW TAF: ' + "`" + body.data[0].raw_text + "`")
+            .setColor('#47970E')
+            .addBlankField()
+            .addField('Forecast', `From: ${moment(body.data[0].timestamp.from).format('DD/MM/YYYY HH:mm')}z to: ${moment(body.data[0].timestamp.to).format('DD/MM/YYYY HH:mm')}`);
+
+          body.data[0].forecast.forEach(forecast => {
+            if (forecast.section_key == 0) return; 
+            console.log(forecast);
+            embed.addBlankField()
+              .addField(`${forecast.change.indicator.text}`, `${forecast.change.indicator.description}`);
+            if (forecast.wind.gust_kts) {
+              embed.addField('Wind', `${forecast.wind.degrees} Degrees ${forecast.wind.speed_kts} Knots ${forecast.wind.speed_mph} Miles per Hour. Gusting  ${forecast.wind.gust_kts} Knots ${forecast.wind.gust_kts} Miles per Hour`, true);
+            } else if (forecast.wind && !forecast.wind.gust_kts) {
+              embed.addField('Wind', `${forecast.wind.degrees} Degrees ${forecast.wind.speed_kts} Knots ${forecast.wind.speed_mph} Miles per Hour.`, true);
+            } else if (forecast.visibility) { 
+              embed.addField('Visibility', `${forecast.visibility.miles} Miles ${forecast.visibility.meters} Meters`, true);
+            } else if (forecast.clouds) {
+              embed.addField('Clouds', `${forecast.clouds.text} ${forecast.clouds.base_feet_agl} Feet ${forecast.clouds.base_meters_agl} Meters`);
+            } else if (forecast.conditions.length > 0) {
+              forecast.conditions.forEach(condition => {
+                embed.addField('Conditions', `${condition.text}`, true);
+              });
             }
-          };
+          });
+          //     fields: [
+          //       {
+          //         name: "Clouds",
+          //         value: body.data[0].feet_agl
+          //           ? `${body.data[0].ceiling.text} ${body.data[0].ceiling.feet_agl} feet ${body.data[0].ceiling.meters_agl} meters`
+          //           : body.data[0].clouds[0]
+          //           ? body.data[0].clouds[0].text
+          //           : `Unknown Details`,
+          //         inline: true
+          //       },
+          //       {
+          //         name: "Temperature",
+          //         value: `${body.data[0].temperature.celsius}°C ${body.data[0].temperature.fahrenheit}°F`,
+          //         inline: true
+          //       },
+          //       {
+          //         name: "Dewpoint",
+          //         value: `${body.data[0].dewpoint.celsius}°C ${body.data[0].dewpoint.fahrenheit}°F`,
+          //         inline: true
+          //       },
+          //       {
+          //         name: "Barometer (QNH)",
+          //         value: `${body.data[0].barometer.hpa} hPa ${body.data[0].barometer.hg} hg`,
+          //         inline: true
+          //       },
+          //       {
+          //         name: "Humidity",
+          //         value: `${body.data[0].humidity.percent}%`,
+          //         inline: true
+          //       }
+          //     ]
+          //   }
+          // };
           return message.channel.send(embed);
         } else {
           return message.reply(
