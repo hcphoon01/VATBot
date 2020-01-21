aa76103c52af47855ac3250d9ef13af443d5946b
Harry Cameron <hcphoon01@gmail.com>
2020-01-21 21:00:27 +0000

    migrated to better-sqlite3 to resolve timeout issues

--- src/cron/AirportDatabaseUpdate.js
+++ src/cron/AirportDatabaseUpdate.js
@@ -1,60 +1,30 @@
 const csv = require("csv-parser");
 const request = require("request");
-const sqlite3 = require("sqlite3");
 const Promise = require("bluebird");
 const path = require("path");
 
 class AirportDatabaseUpdate {
   constructor(dbFilePath) {
-    this.db = new sqlite3.Database(dbFilePath, err => {
-      if (err) {
-        console.log("Could not connect to database", err);
-      } else {
-        console.log("Connected to database");
-      }
-    });
-  }
-
-  // run an sql query
-  run(sql, params = []) {
-    return new Promise((resolve, reject) => {
-      this.db.run(sql, params, function(err) {
-        if (err) {
-          console.log("Error running sql " + sql);
-          console.log(err);
-          reject(err);
-        } else {
-          resolve({ id: this.lastID });
-        }
-      });
-    });
+    this.db = require("better-sqlite3")(dbFilePath);
   }
 
   // create the airports table if it doesnt exist
-  createTable() {
-    const sql = `
+  async createTable() {
+    const sql = this.db.prepare(`
     CREATE TABLE IF NOT EXISTS airports (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       icao STRING UNIQUE,
       latitude INTEGER,
-      longitude INTEGER)`;
-    return this.run(sql);
-  }
-
-  // insert data
-  insert(id, icao, latitude, longitude) {
-    const sql = `
-      INSERT INTO airports (id, icao, latitude, longitude)
-      VALUES (?, ?, ?, ?)`;
-    return this.run(sql, [id, icao, latitude, longitude]);
+      longitude INTEGER)`);
+    return await sql.run();
   }
 
   // fetch last ID
   async fetchLastId() {
-    const sql = `
-      SELECT id FROM airports ORDER BY id DESC LIMIT 0, 1`;
-    const result = await this.run(sql);
-    if (result.id == 0) return 1;
+    const sql = this.db.prepare(`
+      SELECT id FROM airports ORDER BY id DESC LIMIT 0, 1`);
+    const result = await sql.get();
+    if (!result) return 1;
     else {
       return result.id;
     }
@@ -67,36 +37,33 @@ class AirportDatabaseUpdate {
     const file = "http://ourairports.com/data/airports.csv";
     console.log('file fetched');
     var data = [];
+    const insert = this.db.prepare('INSERT INTO airports (id, icao, latitude, longitude) VALUES (?,?,?,?)');
     request(file)
       .pipe(csv())
       .on("data", row => {
-        // this.insert(index, row.ident, row.latitude_deg, row.longitude_deg);
-        // index++;
         data.push(row);
       })
       .on("end", () => {
         console.log("all fetched, starting insert");
-        data.forEach(item => {
-          this.insert(index, item.ident, item.latitude_deg, item.longitude_deg);
-          index++;
-          console.log(index);
+        const bulkInsert = this.db.transaction((list) => {
+          for (const item of list) {
+            insert.run(index, item.ident, item.latitude_deg, item.longitude_deg);
+            index++;
+            console.log(index);
+          }
         });
+        bulkInsert(data);
+        
         console.log("done");
         process.exit(1);
       });
   }
 
-  // rename temp table
-  // async renameTable() {
-  //   const sql = `ALTER TABLE temp_airports RENAME TO airports`;
-  //   await this.run(sql);
-  //   return "renamed";
-  // }
 
   // delete the airports table
   async dropTable() {
-    const sql = `DROP TABLE IF EXISTS airports`;
-    await this.run(sql);
+    const sql = this.db.prepare(`DROP TABLE IF EXISTS airports`);
+    await sql.run();
     return "dropped";
   }
 
