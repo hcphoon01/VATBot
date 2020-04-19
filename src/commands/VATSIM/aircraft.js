2da6167dc2a9e9b056f9f4cdde655cef414a8ae9
Harry Cameron <hcphoon01@gmail.com>
2020-04-19 17:28:26 +0100

    Revert "updated airport command"
    This reverts commit e8b1fcf7fef8b0508f223674ebee035d9f302bb4.

--- src/commands/VATSIM/aircraft.js
+++ src/commands/VATSIM/aircraft.js
@@ -20,24 +20,24 @@ module.exports = class extends Command {
             const display = new RichDisplay(new MessageEmbed()
                 .setTitle(val.callsign)
                 .setColor('#47970E')
-                .setAuthor(val.realname)
+                .setAuthor(val.member.name)
             );
             display.addPage(template => {
-                template.addField('Speed', val.groundspeed, true)
+                template.addField('Speed', val.speed, true)
                 .addField('Altitude', val.altitude, true)
                 .addField('Heading', val.heading, true);
                 return template;
             });
 
-            if (val.planned_aircraft) {
+            if (val.plan) {
                 display.addPage(template => {
-                    template.addField('Departure', val.planned_depairport, true)
-                    .addField('Arrival', val.planned_destairport, true);
-                    if (val.planned_altairport) {
-                        template.addField('Alternate', val.planned_altairport, true);
+                    template.addField('Departure', val.plan.departure, true)
+                    .addField('Arrival', val.plan.arrival, true);
+                    if (val.plan.alternate) {
+                        template.addField('Alternate', val.plan.alternate, true);
                     }
-                    template.addField('Aircraft', val.planned_aircraft)
-                    .addField('Route', val.planned_route);
+                    template.addField('Aircraft', val.plan.aircraft)
+                    .addField('Route', val.plan.route);
 
                     return template;
                 });
