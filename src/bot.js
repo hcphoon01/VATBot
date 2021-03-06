f99ea647717d3e3cffa76fe87c7cf093628baf5f
Harry Cameron <hcphoon01@gmail.com>
2021-03-06 22:50:46 +0000

    feat: initial prefix setting work

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,14 @@
-const { handler } = require('vatsim-data-handler');
-const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo');
-require('dotenv').config();
+const { handler } = require("vatsim-data-handler");
+const {
+  AkairoClient,
+  CommandHandler,
+  ListenerHandler,
+  SQLiteProvider,
+} = require("discord-akairo");
+const sqlite = require("sqlite");
+const sqlite3 = require('sqlite3');
+const path = require("path");
+require("dotenv").config();
 
 // const client = new KlasaClient({
 //   prefix: "!",
@@ -14,44 +22,70 @@ require('dotenv').config();
 // Client.defaultGuildSchema.add('notify_channel', 'string');
 
 class MyClient extends AkairoClient {
-    constructor() {
-        super({
-            // Options for Akairo go here.
-        }, {
-            // Options for discord.js goes here.
-        });
+  constructor() {
+    super(
+      {
+        // Options for Akairo go here.
+      },
+      {
+        // Options for discord.js goes here.
+      }
+    );
 
-        this.commandHandler = new CommandHandler(this, {
-            directory: './src/commands/',
-            prefix: '!'
-        });
+    this.commandHandler = new CommandHandler(this, {
+      directory: "./src/commands/",
+      prefix: (message) => {
+        if (message.guild) {
+          return this.settings.get(message.guild.id, "prefix", "!");
+        }
 
-        this.commandHandler.resolver.addType('icao', (message, phrase) => {
-            if (!phrase) return null;
-            const result = /[A-Z]{4}/i.exec(phrase);
-            if (result) return phrase.toUpperCase();
-            return null;
-        });
+        return "!";
+      },
+    });
 
-        this.listenerHandler = new ListenerHandler(this, {
-            directory: './src/listeners/'
-        });
+    this.commandHandler.resolver.addType("icao", (message, phrase) => {
+      if (!phrase) return null;
+      const result = /[A-Z]{4}/i.exec(phrase);
+      if (result) return phrase.toUpperCase();
+      return null;
+    });
 
-        this.listenerHandler.setEmitters({
-            process: process,
-        });
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: "./src/listeners/",
+    });
 
-        this.commandHandler.loadAll();
-        //this.listenerHandler.loadAll();
-    }
+    this.listenerHandler.setEmitters({
+      process: process,
+    });
+
+    this.commandHandler.loadAll();
+    //this.listenerHandler.loadAll();
+
+    this.settings = new SQLiteProvider(
+      sqlite.open({ filename: path.join(__dirname + "/bwd/sqlite/db.sqlite"), driver: sqlite3.Database }),
+      "guild_settings",
+      {
+        idColumn: "guild_id",
+        dataColumn: "settings",
+      }
+    );
+  }
+  async _init() {
+    await this.settings.init();
+  }
 }
 
 const client = new MyClient();
+client._init();
 client.login(process.env.DISCORD_TOKEN);
 
 client.handler = handler;
 
-client.on('ready', () => {
-    client.handler.getCount('all').then(val => client.user.setActivity(`over ${val} users | !help`, { type: 'WATCHING' }));
-    console.log(`Successfully initialized.`);
+client.on("ready", () => {
+  client.handler
+    .getCount("all")
+    .then((val) =>
+      client.user.setActivity(`over ${val} users | !help`, { type: "WATCHING" })
+    );
+  console.log(`Successfully initialized.`);
 });
