d1f21c3e3cec6f8e8998486d26c331b6f4373f37
Harry Cameron <hcphoon01@gmail.com>
2021-03-14 14:16:30 +0000

    style: more formatting

--- migrations/20210307111754-airports.js
+++ migrations/20210307111754-airports.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return db.createTable('airports', {
-    id: {type: 'int', primaryKey: true},
-    icao: {type: 'string', unique: true},
-    name: {type: 'string'},
-    latitude: {type: 'decimal'},
-    longitude: {type: 'decimal'}
+exports.up = function (db) {
+  return db.createTable("airports", {
+    id: { type: "int", primaryKey: true },
+    icao: { type: "string", unique: true },
+    name: { type: "string" },
+    latitude: { type: "decimal" },
+    longitude: { type: "decimal" },
   });
 };
 
-exports.down = function(db) {
-  return db.dropTable('airports');
+exports.down = function (db) {
+  return db.dropTable("airports");
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
